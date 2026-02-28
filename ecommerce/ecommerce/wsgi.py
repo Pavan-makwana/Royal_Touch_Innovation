36fd50fb00edbf37d61760f0787839090faafb69
Pa1 <pavan561981@gmail.com>
2026-02-28 14:51:41 +0530

    Deployment ready: Added Vercel config, Whitenoise, and .env security

--- ecommerce/ecommerce/wsgi.py
+++ ecommerce/ecommerce/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for ecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
+# Pehle application define karo
 application = get_wsgi_application()
+
+# Phir Vercel ke liye usse 'app' variable mein assign karo
+app = application
