13e89c63bb528afebd6fa6837f676e2daddfcd9e
Pa1 <pavan561981@gmail.com>
2026-02-28 14:59:00 +0530

    a

--- ecommerce/ecommerce/wsgi.py
+++ ecommerce/ecommerce/wsgi.py
@@ -3,8 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-# Pehle application define karo
-application = get_wsgi_application()
-
-# Phir Vercel ke liye usse 'app' variable mein assign karo
-app = application
+application = get_wsgi_application() 
+app = application                    
